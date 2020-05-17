653612be938a351140e961b9f817eb2919a56e5e
钟达 <dalsen_chung@MacBookPro.local>
2020-05-17 22:08:44 +0800

    commit week5 homework

--- week05/client.js
+++ week05/client.js
@@ -40,6 +40,7 @@ ${this.bodyText}`
     }
     send (connection) {
         return new Promise((resolve, reject) => {
+            const parser = new ResponseParser()
             if (connection) {
                 connection.write(this.toString())
             } else {
@@ -50,8 +51,12 @@ ${this.bodyText}`
                     connection.write(this.toString())
                 })
             }
-            connection.on('data', (data) => {
-                resolve(data.toString())
+            connection.on('data', (data) => { // TCP是流式传输 会有沾包的情况
+                parser.receive(data.toString())
+
+                if (parser.isFinished) {
+                    resolve(parser.response)
+                }
                 connection.end()
             })
             connection.on('error', (err) => {
@@ -67,9 +72,147 @@ class Response {
 }
 
 class ResponseParser {
+    constructor () {
+        this.WAITING_STATUS_LINE = 0
+        this.WAITING_STATUS_LINE_END = 1
+        this.WAITING_HEADER_NAME = 2
+        this.WAITING_HEADER_SPACE= 3
+        this.WAITING_HEADER_VALUE = 4
+        this.WAITING_HEADER_LINE_END = 5
+        this.WAITING_HEADER_BLOCK_END = 6
+        this.WAITING_BODY = 7
 
+        this.current = this.WAITING_STATUS_LINE
+        this.statusLine = ''
+        this.headers = {}
+        this.headerName = ''
+        this.headerValue = ''
+        this.bodyParser = null
+    }
+
+    get isFinished () {
+        return this.bodyParser && this.bodyParser.isFinished
+    }
+
+    get response () {
+        this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+        return {
+            statusCode: RegExp.$1,
+            statusText: RegExp.$2,
+            headers: this.headers,
+            body: this.bodyParser.content.join('')
+        }
+    }
+
+    receive (string) {
+        for (let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i))
+        }
+    }
+
+    receiveChar (char) {
+        if (this.current === this.WAITING_STATUS_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_STATUS_LINE_END
+            } else {
+                this.statusLine += char
+            }
+        } else if (this.current === this.WAITING_STATUS_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if (this.current === this.WAITING_HEADER_NAME) {
+            if (char === ':') {
+                this.current = this.WAITING_HEADER_SPACE
+            } else if(char === '\r') {
+                this.current = this.WAITING_HEADER_BLOCK_END
+                if (this.headers['Transfer-Encoding'] === 'chunked') {
+                    this.bodyParser = new TrunkedBodyParser()
+                }
+            } else {
+                this.headerName += char
+            }
+        } else if (this.current === this.WAITING_HEADER_SPACE) {
+            if (char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE
+            }
+        } else if (this.current === this.WAITING_HEADER_VALUE) {
+            if (char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END
+                this.headers[this.headerName] = this.headerValue
+                this.headerName = ''
+                this.headerValue = ''
+            } else {
+                this.headerValue += char
+            }
+        } else if (this.current === this.WAITING_HEADER_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_HEADER_NAME
+            }
+        } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_BODY
+            }
+        } else if (this.current === this.WAITING_BODY) {
+            this.bodyParser.receiveChar(char)
+        }
+    }
 }
 
+class TrunkedBodyParser {
+    constructor () {
+        this.WAITING_LENGTH = 0
+        this.WAITING_LENGTH_LINE_END = 1
+        this.READING_TRUNK = 2
+        this.WAITING_NEW_LINE = 3
+        this.WAITING_NEW_LINE_END = 4
+
+        this.length = 0
+        this.content = []
+        this.isFinished = false
+        this.current = this.WAITING_LENGTH
+    }
+    receiveChar (char) {
+        // console.log(JSON.stringify(char))
+        if (this.current === this.WAITING_LENGTH) {
+            if (char === '\r') {
+                if (this.length === 0) {
+                    this.isFinished = true
+                }
+                this.current = this.WAITING_LENGTH_LINE_END
+            } else {
+                this.length *= 10
+                this.length += char.charCodeAt(0) - '0'.charCodeAt(0) // Number(char)
+            }
+        }
+        else if (this.current === this.WAITING_LENGTH_LINE_END) {
+            if (char === '\n') {
+                this.current = this.READING_TRUNK
+            }
+        }
+        else if (this.current === this.READING_TRUNK) {
+            this.content.push(char)
+            this.length --
+            if (this.length === 0) {
+                this.current = this.WAITING_NEW_LINE
+            }
+        }
+        else if (this.current === this.WAITING_NEW_LINE) {
+            if (char === '\r') {
+                this.current = this.WAITING_NEW_LINE_END
+            }
+        }
+        else if (this.current === this.WAITING_NEW_LINE_END) {
+            if (char === '\n') {
+                this.current = this.WAITING_LENGTH
+            }
+        }
+    }
+}
+
+/**
+ * Start
+ */
 void async function () {
     let request = new Request({
         method: 'POST',
@@ -87,49 +230,3 @@ void async function () {
     let respone = await request.send()
     console.log(respone)
 }()
-
-/*
-const client = net.createConnection({
-    host: '127.0.0.1',
-    port: 8088 }, () => {
-    // 'connect' listener.
-    console.log('connected to server!');
-
-    let request = new Request({
-        method: 'POST',
-        host: '127.0.0.1',
-        port: '8088',
-        path: '/',
-        headers: {
-            ['X-Foo2']: 'customer'
-        },
-        body: {
-            name: 'Dalsen'
-        }
-    })
-    // console.log(request.toString())
-    client.write(request.toString())
-
-    // client.write('POST / HTTP/1.1\r\n');
-    // client.write('HOST: 127.0.0.1\r\n');
-    // client.write('Content-Type: application/x-www-form-urlencoded\r\n');
-    // client.write('Content-Length: 20\r\n');
-    // client.write('\r\n');
-    // client.write('field=aaa&code=x%3D1\r\n');
-    // client.write('\r\n');
-});
-
-client.on('data', (data) => {
-    console.log(data.toString());
-    client.end();
-});
-
-client.on('end', () => {
-    console.log('disconnected from server');
-});
-
-client.on('error', (err) => {
-    console.log(err);
-    client.end()
-});
-*/
